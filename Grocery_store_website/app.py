cb77de1f8100d82e3e42f2db63ffaeee7aa3b93d
Your Name <it23379824@my.sliit.lk>
2025-08-10 12:12:29 +0530

    add new codes

--- Grocery_store_website/app.py
+++ Grocery_store_website/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
 from db_config import init_db
 import routes.product_routes as product_module
 import routes.customer_routes as customer_module
@@ -6,6 +8,10 @@ import routes.order_routes as order_module
 
 app = Flask(__name__)
 mysql = init_db(app)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+# Ensure upload folder exists
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
 # Inject the mysql instance into route modules
 product_module.mysql = mysql
@@ -17,14 +23,41 @@ app.register_blueprint(product_module.product_bp)
 app.register_blueprint(customer_module.customer_bp)
 app.register_blueprint(order_module.order_bp)
 
-# New route to render HTML product page
+@app.route('/add-product', methods=['GET', 'POST'])
+def add_product():
+    if request.method == 'POST':
+        product_name = request.form['product_name']
+        category = request.form['category']
+        price = request.form['price']
+        discount = request.form['discount']
+        image = request.files['image']
+
+        filename = None
+        if image:
+            filename = secure_filename(image.filename)
+            image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            image.save(image_path)
+
+        # Save to MySQL
+        cur = mysql.connection.cursor()
+        cur.execute("""
+            INSERT INTO product (name, category, price, discount, image_url)
+            VALUES (%s, %s, %s, %s, %s)
+        """, (product_name, category, price, discount, filename))
+        mysql.connection.commit()
+        cur.close()
+
+        # Redirect to product list page
+        return redirect(url_for('product_page'))
+
+    return render_template('add_product.html')
+
 @app.route('/products')
 def product_page():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM product")
-    rows = cur.fetchall()
-    columns = [col[0] for col in cur.description]
-    products = [dict(zip(columns, row)) for row in rows]
+    cur.execute("SELECT product_id, name, category, price, discount, image_url FROM product")
+    products = cur.fetchall()
+    cur.close()
     return render_template('product.html', products=products)
 
 if __name__ == '__main__':
--- Grocery_store_website/db_config.py
+++ Grocery_store_website/db_config.py
@@ -1,9 +1,11 @@
 from flask_mysqldb import MySQL
+import MySQLdb.cursors
 
 def init_db(app):
     app.config['MYSQL_HOST'] = '127.0.0.1'
-    app.config['MYSQL_PASSWORD'] ='tharushi@2001'
-    app.config['MYSQL_USER'] = 'root' 
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'tharushi@2001'
     app.config['MYSQL_DB'] = 'grocery_store'
+    app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  
 
     return MySQL(app)
